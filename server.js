9572361cff77a6d4016122ae0bbe0a9a469f3201
rakin <rakinadm19@gmail.com>
2020-10-28 22:54:24 -0700

    Added users list which updates when users enter and leave chat

--- server.js
+++ server.js
@@ -35,6 +35,12 @@ io.on("connection", (socket) => {
         "message",
         formatMessage(botName, `${user.username} has joined the chat`)
       );
+
+    // Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
   });
 
   // Listen for chatMessage
@@ -53,6 +59,10 @@ io.on("connection", (socket) => {
         "message",
         formatMessage(botName, `${user.username} has left the chat`)
       );
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
     }
   });
 });
